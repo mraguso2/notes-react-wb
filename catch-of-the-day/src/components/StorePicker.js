bef090f75d9a9a787666134bd85a4bb86df1a66f
Michael Raguso <michael.raguso@gmail.com>
2017-08-09 21:15:27 -0400

    added style, made more components

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,8 +1,21 @@
 import React from 'react';
 
+/**
+ * You can only return one parent element
+ * Need to make sure you self close tags
+ */
+
 class StorePicker extends React.Component {
+  // use jsx to write html in js
   render() {
-    return <p>Hello</p>
+    return (
+      <form className="store-selector">
+        { /* This is how you comment in jsx */ }
+        <h2>Please Enter A Store</h2>
+        <input type="text" required placeholder="Store Name" />
+        <button type="submit">Visit Store -> </button>
+      </form>
+    )
   }
 }
 
--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,7 +1,9 @@
 // any code that gets written in here will be bundled
 import React from 'react';
 import { render } from 'react-dom'; // just import one method from react-dom
+import './css/style.css'; // importing css from css file
 
 import StorePicker from './components/StorePicker';
+import App from './components/App';
 
-render(<StorePicker/>, document.querySelector('#main'));
+render(<App/>, document.querySelector('#main'));
