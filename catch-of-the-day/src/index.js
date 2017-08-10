419a96608f1fcef6afe6f257aa88bd78810b905e
Michael Raguso <michael.raguso@gmail.com>
2017-08-09 20:43:11 -0400

    first components

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,1 +1,7 @@
-// let's go!
+// any code that gets written in here will be bundled
+import React from 'react';
+import { render } from 'react-dom'; // just import one method from react-dom
+
+import StorePicker from './components/StorePicker';
+
+render(<StorePicker/>, document.querySelector('#main'));
